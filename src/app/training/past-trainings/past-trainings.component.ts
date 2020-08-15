6b50fb5fdfd191d3fe09f31cc2699ce47a9ea6dd
Talha Munir <46424469+mtalham@users.noreply.github.com>
2020-08-16 00:23:42 +0200

    update angular material and cdk

--- src/app/training/past-trainings/past-trainings.component.ts
+++ src/app/training/past-trainings/past-trainings.component.ts
@@ -1,7 +1,10 @@
 import {AfterViewInit, Component, OnDestroy, OnInit, ViewChild} from '@angular/core';
 import {TrainingService} from '../training.service';
 import {Exercise} from '../exercise.model';
-import {MatDialog, MatPaginator, MatSort, MatTableDataSource} from '@angular/material';
+import { MatDialog } from '@angular/material/dialog';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
 import {Subscription} from 'rxjs';
 import {TrainingConfirmationComponent} from '../training-confirmation.component';
 import {UIService} from '../../shared/UI.service';
